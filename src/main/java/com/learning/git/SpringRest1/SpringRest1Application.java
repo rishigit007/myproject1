f1b363ea4085b8d0ac01430aedf7773fa33246d9
indranil goswami <indranil.goswami14@gmail.com>
2018-06-28 18:17:51 +0530

    first commit in cmd

---
+++ src/main/java/com/learning/git/SpringRest1/SpringRest1Application.java
@@ -0,0 +1,12 @@
+package com.learning.git.SpringRest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRest1Application.class, args);
+	}
+}
---
+++ src/main/java/com/learning/git/SpringRest1/SpringRestApplication.java
@@ -0,0 +1,12 @@
+package com.learning.git.SpringRest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApplication.class, args);
+	}
+}
